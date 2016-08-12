caef0f3e4b0e5e3e9e64ca2baafea088ed8d1173
Jianeng Xu <xujianeng0079@gmail.com>
2016-08-12 15:39:44 +0800

    Add more comments

--- GetPrice.py
+++ GetPrice.py
@@ -23,22 +23,22 @@ from TableFunction import *
 # 12. 临沂和南京粉料
 
 
-# Login info.
+# 账号登录信息
 OilchemUrlBase = r"http://www.oilchem.net/"
 OilchemUser = "rockyeah"
 OilchemPass = "abc123"
 
-# Data URL
+# 数据网址
 Dm = r"http://price.oilchem.net/dmPrice/listProduct.lz?"
 Im = r"http://price.oilchem.net/imPrice/listProduct.lz?"
 De = r"http://price.oilchem.net/depProList.lz?"
 PP = r"http://plas.oilchem.net/330/208/318/"
 
-# Date
+# 日期
 today = datetime.datetime.now()
 today = today.strftime("%m月%d日")
 
-# File name
+# 文件名
 fname = today + "价格信息.txt"
 
 #####################################################################################
@@ -56,10 +56,12 @@ LoginData = {}
 LoginData["username"] = OilchemUser
 LoginData["password"] = OilchemPass
 
-# Login
+# 登录
 GetOneURLWithCookie(LoginURL, opener, False, LoginData)
 
-# Get Data Page
+print "正在登录并获取网页..."
+
+# 获取数据网页
 Data = {}
 # 丙烯：华东地区及山东地区
 Data["pName"] = "丙烯"
@@ -111,205 +113,265 @@ Page10 = GetOneURLWithCookie(Dm, opener, True, Data)
 Data["pName"] = "BOPP"
 Page11 = GetOneURLWithCookie(De, opener, True, Data)
 
-# 临沂和南京粉料
-Page12 = GetOneURLWithCookie(PP, opener, True, Data)
+# PP煤化工价(华东的中煤榆林L5E89和宁波富德T30S)：
+Data["pName"] = "PP粒"
+Page12 = GetOneURLWithCookie(Dm, opener, True, Data)
 
-# Logout
-LogoutURL = OilchemUrlBase + r"user/exit.shtml"
-GetOneURLWithCookie(LogoutURL, opener, True)
+# 临沂和南京粉料
+Page13 = GetOneURLWithCookie(PP, opener, True, Data)
+
+print "网页获取成功"
 
 #####################################################################################
 
+# 新建txt文件
 fp = open(fname.decode("utf8"), 'w')
 
 #####################################################################################
 
+# 基本步骤:
+# 1. 找到表格
+# 2. 找到需要获取的行(从0开始计数)
+# 3. 将数据写入txt
+
+# FindTableData、Row、PrintTable、WriteRow等函数在TableFunction.py中定义
+
+######################################################
 # 1.丙烯：华东地区及山东地区
 region = ("华东地区", "山东地区")
 for word in region:
-    # 获取表格中的数据
-    (Header, Data, Info) = FinTableData(word, Page1, 1)
-    fname = "丙烯华东地区及山东地区" + "Table.txt"
+    (Header, Data, Info) = FindTableData(word, Page1, 1)
+
+    # fname = "丙烯华东地区及山东地区" + "Table.txt"
     # PrintTable(Header, Data, Info, fname)
-    # 输出表格
+
     RowNumber = Row(Info, "丙烯")
     WriteRow(fp, word, Header, 2, Data, 1, range(RowNumber), Info, 2, 1)
 
-print "获取丙烯：华东地区及山东地区..."
+print "写入丙烯：华东地区及山东地区..."
+
+######################################################
 
 # 2.LLDPE中国地区(前一天的数据)
-# 获取表格中的数据
-(Header, Data, Info) = FinTableData("中国地区", Page2, 2)
-fname = "LLDPE中国地区(前一天的数据)" + "Table.txt"
+(Header, Data, Info) = FindTableData("中国地区", Page2, 2)
+
+#fname = "LLDPE中国地区(前一天的数据)" + "Table.txt"
 # PrintTable(Header, Data, Info, fname)
-# 输出表格
+
 RowNumber = Row(Info, "LLDPE(丁烯基)")
 WriteRow(fp, "中国地区", Header, 2, Data, 2, range(RowNumber), Info, 2, 1)
 
-print "获取LLDPE中国地区(前一天的数据)..."
+print "写入LLDPE中国地区(前一天的数据)..."
+
+######################################################
 
 # 3.LDPE中国地区(前一天的数据)
-# 获取表格中的数据
-(Header, Data, Info) = FinTableData("中国地区", Page3, 2)
-fname = "LDPE中国地区(前一天的数据)" + "Table.txt"
-PrintTable(Header, Data, Info, fname)
-# 输出表格
+(Header, Data, Info) = FindTableData("中国地区", Page3, 2)
+
+# fname = "LDPE中国地区(前一天的数据)" + "Table.txt"
+# PrintTable(Header, Data, Info, fname)
+
 RowNumber = Row(Info, "LDPE(通用)")
 WriteRow(fp, "中国地区", Header, 2, Data, 2, range(RowNumber), Info, 2, 1)
 
-print "获取LDPE中国地区(前一天的数据)..."
+print "写入LDPE中国地区(前一天的数据)..."
+
+######################################################
 
 # 4.HDPE(注塑)中国地区(前一天的数据)
-(Header, Data, Info) = FinTableData("中国地区", Page4, 2)
-fname = "HDPE(注塑)中国地区(前一天的数据)" + "Table.txt"
+(Header, Data, Info) = FindTableData("中国地区", Page4, 2)
+
+# fname = "HDPE(注塑)中国地区(前一天的数据)" + "Table.txt"
 # PrintTable(Header, Data, Info, fname)
-# 输出表格
+
 Row = [2]
 WriteRow(fp, "中国地区", Header, 2, Data, 2, Row, Info, 2, 1)
 
-print "获取HDPE(注塑)中国地区(前一天的数据)..."
+print "写入HDPE(注塑)中国地区(前一天的数据)..."
+
+######################################################
 
 # 5.PP(均聚、共聚)中国地区(前一天的数据)
-(Header, Data, Info) = FinTableData("中国地区", Page5, 2)
-fname = "PP(均聚、共聚)中国地区(前一天的数据)" + "Table.txt"
+(Header, Data, Info) = FindTableData("中国地区", Page5, 2)
+
+# fname = "PP(均聚、共聚)中国地区(前一天的数据)" + "Table.txt"
 # PrintTable(Header, Data, Info, fname)
-# 输出表格
+
 Row = [2, 3]
 WriteRow(fp, "中国地区", Header, 2, Data, 2, Row, Info, 2, 1)
 
-print "获取PP(均聚、共聚)中国地区(前一天的数据)..."
+print "写入PP(均聚、共聚)中国地区(前一天的数据)..."
+
+######################################################
+
 
 # 6.LLDPE价格(中油华北的大庆和吉林7042、中石化华北的齐鲁7042、中油华东的大庆7042、
 # 中石化华东的扬子和镇海7042、中油华南的大庆和吉林7042、中石化华南的福建联合和茂名石化)
 # 华北
-(Header, Data, Info) = FinTableData("华北地区", Page6, 1)
-fname = "LLDPE出厂价华北" + "Table.txt"
+(Header, Data, Info) = FindTableData("华北地区", Page6, 1)
+
+# fname = "LLDPE出厂价华北" + "Table.txt"
 # PrintTable(Header, Data, Info, fname)
-# 输出表格
+
 Row = [0, 2, 3]
 WriteRow(fp, "华北地区", Header, 4, Data, 1, Row, Info, 4, 2)
 
 # 华东
-(Header, Data, Info) = FinTableData("华东地区", Page6, 1)
-fname = "LLDPE出厂价华东" + "Table.txt"
+(Header, Data, Info) = FindTableData("华东地区", Page6, 1)
+
+#fname = "LLDPE出厂价华东" + "Table.txt"
 # PrintTable(Header, Data, Info, fname)
-# 输出表格
+
 Row = [0, 8, 9]
 WriteRow(fp, "华东地区", Header, 4, Data, 1, Row, Info, 4, 2)
 
 # 华南
-(Header, Data, Info) = FinTableData("华南地区", Page6, 1)
-fname = "LLDPE出厂价华南" + "Table.txt"
+(Header, Data, Info) = FindTableData("华南地区", Page6, 1)
+
+# fname = "LLDPE出厂价华南" + "Table.txt"
 # PrintTable(Header, Data, Info, fname)
-# 输出表格
+
 Row = [0, 2, 3, 6, 8, 9]
 WriteRow(fp, "华南地区", Header, 4, Data, 1, Row, Info, 4, 2)
 
-print "获取LLDPE企业出厂价..."
+print "写入LLDPE企业出厂价..."
+
+######################################################
 
 # 7.LDPE价格(中油华东的大庆2426H、中石化华东的茂名2426H)
-(Header, Data, Info) = FinTableData("华东地区", Page7, 1)
-fname = "LDPE华东" + "Table.txt"
+(Header, Data, Info) = FindTableData("华东地区", Page7, 1)
+
+# fname = "LDPE华东" + "Table.txt"
 # PrintTable(Header, Data, Info, fname)
-# 输出表格
+
 Row = [0, 3]
 WriteRow(fp, "华东地区", Header, 4, Data, 1, Row, Info, 4, 2)
 
-print "获取LDPE企业出厂价..."
+print "写入LDPE企业出厂价..."
+
+######################################################
+
 
 # 8.PP价格(中油华北的大庆炼化T30S、中石化华北的济南和齐鲁、中油华东的大庆T30S、
 # 中油华东的独山子K8003、中石化华东的九江和镇海T30S、中油华南的大庆和T30S、
 # 中石化华南的福建联合和海南炼厂和茂名石化T30S)
 # 华北
-(Header, Data, Info) = FinTableData("华北地区", Page8, 1)
-fname = "PP粒华北" + "Table.txt"
+(Header, Data, Info) = FindTableData("华北地区", Page8, 1)
+
+# fname = "PP粒华北" + "Table.txt"
 # PrintTable(Header, Data, Info, fname)
-# 输出表格
+
 Row = [4, 7, 8, 11, 12, 13, 14]
 WriteRow(fp, "华北地区", Header, 4, Data, 1, Row, Info, 4, 2)
 
 # 华东
-(Header, Data, Info) = FinTableData("华东地区", Page8, 1)
-fname = "PP粒华东" + "Table.txt"
+(Header, Data, Info) = FindTableData("华东地区", Page8, 1)
+
+# fname = "PP粒华东" + "Table.txt"
 # PrintTable(Header, Data, Info, fname)
-# 输出表格
+
 Row = [2, 3, 4, 29]
 WriteRow(fp, "华东地区", Header, 4, Data, 1, Row, Info, 4, 2)
 
 # 华南
-(Header, Data, Info) = FinTableData("华南地区", Page8, 1)
-fname = "PP粒华南" + "Table.txt"
+(Header, Data, Info) = FindTableData("华南地区", Page8, 1)
+
+# fname = "PP粒华南" + "Table.txt"
 # PrintTable(Header, Data, Info, fname)
-# 输出表格
+
 Row = [4, 7, 15, 22]
 WriteRow(fp, "华南地区", Header, 4, Data, 1, Row, Info, 4, 2)
 
-print "获取PP粒国内企业出厂价..."
+print "写入PP粒国内企业出厂价..."
+
+######################################################
 
 # 9.PP粉料厂价(华北临沂)
-(Header, Data, Info) = FinTableData("华北地区", Page9, 1)
-fname = "PP粉料华北" + "Table.txt"
+(Header, Data, Info) = FindTableData("华北地区", Page9, 1)
+
+# fname = "PP粉料华北" + "Table.txt"
 # PrintTable(Header, Data, Info, fname)
-# 输出表格
+
 Row = [0]
 WriteRow(fp, "华北地区", Header, 4, Data, 1, Row, Info, 4, 1)
 
 # 10.LLDPE煤化工价(华北的陕西中煤7042)
-(Header, Data, Info) = FinTableData("华北地区", Page10, 1)
-fname = "LLDPE市场价华北" + "Table.txt"
+(Header, Data, Info) = FindTableData("华北地区", Page10, 1)
+
+# fname = "LLDPE市场价华北" + "Table.txt"
 # PrintTable(Header, Data, Info, fname)
-# 输出表格
+
 Row = [3]
 WriteRow(fp, "华北地区", Header, 4, Data, 1, Row, Info, 4, 1)
 
-print "获取PP粉料国内市场价..."
+print "写入PP粉料国内市场价..."
+
+######################################################
 
 # 11.BOPP价格(华东厚光膜江苏双良)
 # 华东
-(Header, Data, Info) = FinTableData("华东地区", Page11, 1)
-fname = "BOPP华东" + "Table.txt"
+(Header, Data, Info) = FindTableData("华东地区", Page11, 1)
+
+#fname = "BOPP华东" + "Table.txt"
 # PrintTable(Header, Data, Info, fname)
-# 输出表格
+
 Row = [8]
 WriteRow(fp, "华东地区", Header, 4, Data, 1, Row, Info, 4, 2)
 
-print "获取BOPP企业出厂价..."
+print "写入BOPP企业出厂价..."
 
 #####################################################################################
 
-fp.write("PP粉市场信息:\n\n")
+
+# 12.PP煤化工价(华东的中煤榆林L5E89和宁波富德T30S)：
+# 华东
+(Header, Data, Info) = FindTableData("华东地区", Page12, 1)
+
+# fname = "PP煤化工价" + "Table.txt"
+# PrintTable(Header, Data, Info, fname)
+
+Row = [2, 4]
+WriteRow(fp, "华东地区", Header, 4, Data, 1, Row, Info, 4, 1)
+
+print "写入PP煤化工价..."
+
+#####################################################################################
+
 # 临沂和南京粉料
-# Data format
+fp.write("PP粉市场信息:\n\n")
+
+# 匹配价格
 pattern1 = "(\d*-\d*元/吨)"
 pattern1 = re.compile(pattern1)
 
+# 匹配发布日期
 datepattern = "发布时间：([\S]*)\s"
 datepattern = re.compile(datepattern)
 
-# Specify Citites
+# 匹配城市
 region = ["南京", "临沂"]
 org = "(?:<li><a href=\")([.\S]*)\"\starget=\"_blank\"\stitle=\"\[PP粉\][.\S]*"
 
 # Get Data for each City
 for city in region:
 
-    # Get the News Link
+    # 获取链接地址
     pattern = org + city
     pattern = re.compile(pattern)
-    match = re.findall(pattern, Page12)
+    match = re.findall(pattern, Page13)
 
     if (match == None):
         print "1:Nothing is found in the page, check the page format!"
     url = match[0]
 
-    # Get the News Page
+    # 进入对应网页
     newspage = GetOneURLWithCookie(url, opener, True)
     datematch = re.findall(datepattern, newspage)
     date = datematch[0]
 
     match1 = re.findall(pattern1, newspage)
 
-    # Output Data
+    # 写入txt文件
     fp.write(city + "\t" + date + "\n")
 
     if city == "南京":
@@ -322,3 +384,11 @@ for city in region:
 #####################################################################################
 
 fp.close()
+
+print "文件写入完毕"
+
+# 退出登录
+LogoutURL = OilchemUrlBase + r"user/exit.shtml"
+GetOneURLWithCookie(LogoutURL, opener, True)
+
+print "退出登录..."
--- TableFunction.py
+++ TableFunction.py
@@ -4,6 +4,11 @@ import re
 
 # 打印表格数据
 def PrintTable(header, data, info, fname):
+    # 输入表格的Header表头(第一行)、Data价格信息(数据)、Info产品信息(规格、厂商等)、文件名
+    # 以上参数除文件名外,由FindTableData函数获得
+    # 新建包含以上信息的txt文件,用于寻找数据位置及查错
+
+
     fp = open(fname.decode("utf8"), 'w')
 
     # Print Header
@@ -31,17 +36,26 @@ def PrintTable(header, data, info, fname):
     fp.close()
 
 
-# 匹配表格数据
-def FinTableData(Region, Page, flag):
+# 获取表格数据
+def FindTableData(Region, Page, flag):
+    # 输入地区、网页
+    # flag=1: 国内价格(数据无超链接)
+    # flag=2: 国际价格(数据有超链接)
+    # 输出表格的Header表头(第一行)、Data价格信息(数据)、Info产品信息(规格、厂商等)
+
     # 匹配公式
+    # 表格
     TablePattern1 = r"(?:<table ){1}[\d\D]*?(?:"
     TablePattern2 = r"){1}[\d\D]*?(?:</table>){1}"
     TablePattern3 = r"(?:<table ){1}[\d\D]*?(?:</table>){1}"
 
+    # 表头
     HeaderPattern = r"<strong>([\S]*)</strong>"
 
+    # 价格信息
     DataPattern = r">\s*(?!7042)([%|\-|0-9|.]+)\s*(?!</a>&nbsp)<"
 
+    # 产品信息
     InfoPattern1 = '''blank">([.\S]*)</a>'''
     InfoPattern2 = '''blank">([^-0-9]*)</a>'''
 
@@ -97,6 +111,8 @@ def FinTableData(Region, Page, flag):
 
 # 统计表格行数
 def Row(Info, word):
+    # 输入表格的Info(由FindTableData函数获得)和产品名称(如"LLDPE"等出现在表格第一列中的名称)
+    # 输出表格行数(不包括表头)
     num = 0
     for temp in Info:
         if temp == word:
@@ -105,7 +121,21 @@ def Row(Info, word):
 
 
 # 输出表格中的某几行
-def WriteRow(fp, Region, Header, HeaderStart, Data, RMB, Row, Info, InfoStep, Priceflag, InfoStart=0):
+def WriteRow(fp, Region, Header, HeaderStart, Data, RMB, Row, Info, InfoStep, Priceflag):
+    # fp: txt文件
+    # Region: 地区名称
+    # Header: 表头(由FindTableData函数获得)
+    # HeaderStart: 第一个日期在表头中的位置
+    # Data: 价格(由FindTableData函数获得)
+    # RMB: 国内价格 RMB=1; 国际价格(表格中有人民币价格) RMB=2
+    # ROW: 需要写入txt的行(从0开始计数)
+    # Info: 产品信息(由FindTableData函数获得)
+    # InfoStep: 产品信息所占列数
+    # Priceflag: 一个日期对应三个价格(主流、底端、高端) Priceflag=1; 一个日期对应一个价格 Priceflag=2
+    # 将所需数据写入fp对应的txt文件
+
+
+    # DataStep: 每一行中价格数据所占的列数(包括涨跌、人民币价)
     if RMB == 1:
         if Priceflag == 1:
             DataStep = 3 * 5 + 2
@@ -120,16 +150,22 @@ def WriteRow(fp, Region, Header, HeaderStart, Data, RMB, Row, Info, InfoStep, Pr
     if Priceflag == 1:
         for rn in Row:
 
-            tempInfo = Info[(InfoStart + rn * InfoStep):(InfoStart + (rn + 1) * InfoStep)]
+            # 找到并写入每一行的产品信息
+            tempInfo = Info[(rn * InfoStep):((rn + 1) * InfoStep)]
             fp.write(Region + ":" + ":".join(tempInfo) + "\n\n")
 
+            # 写入表头
             fp.write("日期\t\t主流价\t低端价\t高端价\t涨跌差\t涨跌率\n\n")
             i = 0
             while (i < 5):
+                # 写入价格
                 fp.write(Header[HeaderStart + i] + "\t\t" + Data[rn * DataStep + i * 3] + "\t" \
                          + Data[rn * DataStep + i * 3 + 1] + "\t" + Data[rn * DataStep + i * 3 + 2] + "\t")
                 if i == 4:
+                    # 写入涨跌
                     fp.write(Data[rn * DataStep + 15] + "\t" + Data[rn * DataStep + 16])
+                    if RMB == 2:
+                        fp.write("\n\n人民币价:\t" + Data[rn * DataStep + 17])
                 fp.write("\t\n\n")
                 i += 1
 
@@ -138,7 +174,7 @@ def WriteRow(fp, Region, Header, HeaderStart, Data, RMB, Row, Info, InfoStep, Pr
     if Priceflag == 2:
         for rn in Row:
 
-            tempInfo = Info[(InfoStart + rn * InfoStep):(InfoStart + (rn + 1) * InfoStep)]
+            tempInfo = Info[(rn * InfoStep):((rn + 1) * InfoStep)]
             fp.write(Region + ":" + ":".join(tempInfo) + "\n\n")
 
             fp.write("日期\t\t价格\t涨跌差\t涨跌率\n\n")
@@ -147,6 +183,8 @@ def WriteRow(fp, Region, Header, HeaderStart, Data, RMB, Row, Info, InfoStep, Pr
                 fp.write(Header[HeaderStart + i] + "\t\t" + Data[rn * DataStep + i] + "\t")
                 if i == 4:
                     fp.write(Data[rn * DataStep + 5] + "\t" + Data[rn * DataStep + 6])
+                    if RMB == 2:
+                        fp.write("\n\n人民币价:\t" + Data[rn * DataStep + 7])
                 fp.write("\t\n\n")
                 i += 1
 
